5438cf698d8bbe3f0288f8f518d73317a65db574
Engin Gültekin <engingultekin1071@gmail.com>
2023-06-11 18:39:39 +0300

    Update reverse_string_with_array.c

--- reverse_string_with_array.c
+++ reverse_string_with_array.c
@@ -6,16 +6,17 @@ int main(void)
 {
     // take a string
     string text = get_string("Enter the text: ");
-    char array[strlen(text)];
+    int len = strlen(text);
+    char array[len];
 
     // assign string to array
-    for(int i = 0; i<strlen(text); i++){
+    for(int i = 0; i < len; i++){
         array[i] = text[i];
     }
     
     //just print the string in reverse
     printf("Reverse Text: ");
-    for(int i = sizeof(array)-1; i>=0; i--){
+    for(int i = (len-1); i >= 0; i--){
         printf("%c",array[i]);
     }
     printf("\n");
