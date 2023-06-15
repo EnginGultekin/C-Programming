c817622bdc931d29e1f20ec1633bebb9e70b7cce
Engin Gültekin <engingultekin1071@gmail.com>
2023-06-15 08:38:19 +0300

    Build a difficult pyramid for Mario

---
+++ mario_advanced_task.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    int number;
+    do
+    {
+        number = get_int("Height: ");
+    } while (number > 8 || number < 1);
+
+    for (int i = 1; i <= number; i++)
+    {
+        for (int j = number; j > 0; j--)
+        {
+            if (j > i)
+            {
+                printf(" ");
+            }
+            else
+            {
+                printf("#");
+            }
+        }
+        printf(" ");
+        for (int j = i; j > 0; j--)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
